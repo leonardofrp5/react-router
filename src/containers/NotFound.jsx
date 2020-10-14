f7d85132af072904e890248ca40c4c8cd160fd8e
leonardofrp5 <leonardofrp5@gmail.com>
2020-10-14 05:51:33 -0500

    some examples whit router

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss';
+
+const NotFound = () => (
+  <div  className="container">
+    <span className="title">404</span>
+    <span className="second__title">Página no encontrada</span>
+  </div>
+)
+
+export default NotFound;
